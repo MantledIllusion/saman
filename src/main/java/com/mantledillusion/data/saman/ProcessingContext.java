81906c7a7e23ffe10e83a38788e1338e40859298
tobias.weber <tobias.weber@ecotel.de>
2018-11-15 11:36:54 +0100

    JavaDoc

--- src/main/java/com/mantledillusion/data/saman/ProcessingContext.java
+++ src/main/java/com/mantledillusion/data/saman/ProcessingContext.java
@@ -16,35 +16,37 @@ public class ProcessingContext {
 			this.context.putAll(base.context);
 		}
 	}
-	
+
 	/**
 	 * Factory method, creates a new {@link ProcessingContext} with the given
 	 * 
 	 * @param contextValues
-	 * @return
+	 *            Optional context values that should be included in the context by
+	 *            their {@link Class} type; might be null, might not contain nulls.
+	 * @return A new {@link ProcessingContext} instance, never null
 	 */
-	public static ProcessingContext of (Object... contextValues) {
+	public static ProcessingContext of(Object... contextValues) {
 		ProcessingContext context = new ProcessingContext();
 		if (contextValues != null) {
 			Arrays.stream(contextValues).forEach(value -> context.set(value));
 		}
 		return context;
 	}
-	
+
 	public <T> boolean has(Class<T> valueType) {
 		return this.context.containsKey(valueType);
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public <T> T get(Class<T> valueType) {
 		if (valueType == null) {
 			throw new IllegalArgumentException("No context value available for a null value type");
 		} else if (!this.context.containsKey(valueType)) {
-			throw new IllegalStateException("No context value available for type "+valueType.getName());
+			throw new IllegalStateException("No context value available for type " + valueType.getName());
 		}
 		return (T) this.context.get(valueType);
 	}
-	
+
 	public <T> ProcessingContext set(T value) {
 		if (value == null) {
 			throw new IllegalArgumentException("Cannot set a null context value");
@@ -52,14 +54,14 @@ public class ProcessingContext {
 		this.context.put(value.getClass(), value);
 		return this;
 	}
-	
+
 	public <T> void remove(Class<T> valueType) {
 		if (valueType == null) {
 			throw new IllegalArgumentException("Cannot remove a context value by a null value type");
 		}
 		this.context.remove(valueType);
 	}
-	
+
 	public void clear() {
 		this.context.clear();
 	}
